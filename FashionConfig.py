b9ed1e1a2f697aa3eb52e1786b501f857427e646
slin <slintw83@gmail.com>
2019-08-28 07:33:19 -0600

    training

--- FashionConfig.py
+++ FashionConfig.py
@@ -10,7 +10,16 @@ class FashionConfig(Config):
 
     RPN_ANCHOR_SCALES = (32, 64, 128, 256, 512)
 
-    LEARNING_RATE = 0.002
+    LEARNING_RATE = 0.001
+    LEARNING_MOMENTUM = 0.9
+
+    LOSS_WEIGHTS = {
+        "rpn_class_loss": 1.,
+        "rpn_bbox_loss": 1.,
+        "mrcnn_class_loss": 1.,
+        "mrcnn_bbox_loss": 1.,
+        "mrcnn_mask_loss": 1.
+    }
 
 class InferenceConfig(FashionConfig):
     GPU_COUNT = 1
--- train.py
+++ train.py
@@ -15,10 +15,11 @@ train_dataset.prepare()
 val_dataset.prepare()
 
 model = modellib.MaskRCNN(mode='training', config=config, model_dir='results')
-model.load_weights('mask_rcnn_fashionbot_0020.h5', by_name=True)
+model.load_weights('mask_rcnn_fashionbot_0005.h5', by_name=True)
 
 augmentation = imgaug.augmenters.Sometimes(p=0.5, then_list=[
     imgaug.augmenters.Fliplr(0.5)
     ])
-config.LEARNING_RATE = 0.0001
-model.train(train_dataset, val_dataset, learning_rate=config.LEARNING_RATE, epochs=20, layers='all', augmentation=augmentation)
+LEARNING_RATE = 0.001
+LEARNING_MOMENTUM = 0.5
+model.train(train_dataset, val_dataset, learning_rate=config.LEARNING_RATE, epochs=20, layers='5+', augmentation=augmentation)
