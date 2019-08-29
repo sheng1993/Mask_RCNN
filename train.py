3343c442a9d1523c2198c57026110d4bb9dbd59f
slin <slintw83@gmail.com>
2019-08-28 19:34:54 -0600

    train

--- train.py
+++ train.py
@@ -15,13 +15,13 @@ train_dataset.prepare()
 val_dataset.prepare()
 
 model = modellib.MaskRCNN(mode='training', config=config, model_dir='results')
-model.load_weights(model.find_last(), by_name=True)
+model.load_weights('mask_rcnn_fashionbot_0015.h5', by_name=True)
 
 augmentation = imgaug.augmenters.Sometimes(p=0.5, then_list=[
     imgaug.augmenters.Fliplr(0.5),
     imgaug.augmenters.Affine()
     ])
-config.LEARNING_RATE = 0.001
+config.LEARNING_RATE = 0.0001
 config.LEARNING_MOMENTUM = 0.5
-config.WEIGHT_DECAY = 0.001
+config.WEIGHT_DECAY = 0.005
 model.train(train_dataset, val_dataset, learning_rate=config.LEARNING_RATE, epochs=40, layers='4+', augmentation=augmentation)
