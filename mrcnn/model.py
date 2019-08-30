ea3d36219fe31f6546d19b237adea4eecadffc21
slin <slintw83@gmail.com>
2019-08-30 07:36:18 -0600

    workers

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2370,7 +2370,7 @@ class MaskRCNN():
             validation_data=val_generator,
             validation_steps=self.config.VALIDATION_STEPS,
             max_queue_size=100,
-            workers=10,
+            workers=workers,
             use_multiprocessing=True,
             verbose=1
         )
